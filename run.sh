40b8550454d1fad48cf71d9947dea01bd5c5c0a0
Will Schenk <wschenk@gmail.com>
2025-06-27 10:54:27 -0400

    fix(scripts): improve script handling and add debug flag
    - Add DEBUG=1 flag to server script to enable debugging information
    - Fix proper quoting of arguments in run.sh to handle spaces and special characters

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm --silent run dev $@
+npm --silent run dev "$@"
 
 # https://www.youtube.com/watch?v=b5EP6YHMaEg
 # curl -X POST http://localhost:8080/jobs -H 'Content-Type: application/json' -d '{"args": ["https://www.youtube.com/watch?v=b5EP6YHMaEg"], "mime_type" : "text/plain"}'
